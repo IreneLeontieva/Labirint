0af8c6d049c9389e0a6c9e29a7bd985d5440fae3
Ирина Леонтьева <i.leontyeva@mafin.ru>
2022-05-13 23:50:19 +0300

    правки

---
+++ src/test/java/helpers/DriverSettings.java
@@ -0,0 +1,28 @@
+package helpers;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import config.Credentials;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class DriverSettings {
+
+    public static void configure() {
+        Configuration.baseUrl = "https://www.labirint.ru/";
+        Configuration.browserSize = System.getProperty("size", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.pageLoadTimeout = 80000;
+
+        if (Credentials.isRemoteWebDriver()) {
+            String user = Credentials.config.user();
+            String password = Credentials.config.password();
+            String remote = Credentials.config.remote();
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+            Configuration.remote = "https://" + user + ":" + password + "@" + remote;
+        }
+    }
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Selenide.open;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.Credentials;
 import helpers.Attach;
+import helpers.DriverSettings;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -29,22 +30,7 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-        Configuration.baseUrl = "https://www.labirint.ru/";
-        Configuration.browserSize = System.getProperty("size", "1920x1080");
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.pageLoadTimeout = 80000;
-
-        if (Credentials.isRemoteWebDriver()) {
-            String user = Credentials.config.user();
-            String password = Credentials.config.password();
-            String remote = Credentials.config.remote();
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.browserCapabilities = capabilities;
-            Configuration.remote = "https://" + user + ":" + password + "@" + remote;
-        }
+        DriverSettings.configure();
     }
 
     @BeforeEach
